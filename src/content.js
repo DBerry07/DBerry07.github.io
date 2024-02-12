d31061c7c5780af6da3eea8ba5e577321b5a88bf
David Berry <dwjberry07@gmail.com>
2024-02-12 14:36:39 -0500

    Create content.js
    Added some text descriptors.

---
+++ src/content.js
@@ -0,0 +1,13 @@
+export const content = [
+    [
+        "I am a software developer currently located in South Florida with nearly two years of professsional native Android app development experience.",
+        "During my professional career, I have worked for Bank of America and for Accenture.",
+        "I earned a Bachelor of Science in Computer Science from Florida Atlantic University in 2019.",
+        "I have been described as detail-oriented and analytical.",
+        "My passion for software development stems from my enjoyment of puzzle-solving and of building functional things.",
+        "I am always eager to learn new things, particularly if those new things help me get better at my job."
+    ],
+    [
+        
+    ]
+]
