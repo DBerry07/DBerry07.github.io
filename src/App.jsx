48b53268729c8758f3f55cc418467972cc2c88f4
David Berry <dwjberry07@gmail.com>
2024-02-13 18:28:45 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./css/App.css";
 import Entry from "./components/Entry.jsx";
+import { titles } from "./data/content.js";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -8,8 +9,8 @@ function App() {
   return (
     <div className="App">
       <menu>
-        <button onClick={() => setCount(0)}>Hello!</button>
-        <button onClick={() => setCount(1)}>Coursera</button>
+        <button onClick={() => setCount(0)}>{titles[0]}</button>
+        <button onClick={() => setCount(1)}>{titles[1]}</button>
       </menu>
       <div id="content-box">
         <Entry index={count} />
