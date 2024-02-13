532605bcab83f940954004317640382e5a85080e
David Berry <dwjberry07@gmail.com>
2024-02-13 15:23:16 -0500

    Updated prebuilt data with my data

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+import "./App.css";
+import * as content from "./content";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      {Entry(0)}
+      {Entry(1)}
+    </div>
+  );
 }
 
-export default App
+function Entry(index) {
+  return (
+    <div>
+      <h2>{content.titles[index]}</h2>
+      <div>
+        {content.text[index].map((item) => (
+          <p> {item}</p>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
