11c4d13ff5b4ad61f86fb69ce65f1117962cdc29
David Berry <dwjberry07@gmail.com>
2024-02-13 15:30:32 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
 import "./App.css";
 import * as content from "./content";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
       {Entry(0)}
