2f2ba34d6e7cfdc69523d1797661245bd6cbcc37
David Berry <dwjberry07@gmail.com>
2024-02-14 09:57:08 -0500

    Changed how buttons were generated/shown

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,14 @@ import { useState } from "react";
 import "./css/App.css";
 import Entry from "./components/Entry.jsx";
 import * as content from "./data/content.js";
+import ButtonMenu from "./components/ButtonMenu.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <menu>
-        <button onClick={() => setCount(0)}>
-          {content.pageContent[0].title}
-        </button>
-        <button onClick={() => setCount(1)}>
-          {content.pageContent[1].title}
-        </button>
-        <button onClick={() => setCount(2)}>
-          {content.pageContent[2].title}
-        </button>
-      </menu>
+      <ButtonMenu />
       <div id="content-box">
         <Entry index={count} />
       </div>
