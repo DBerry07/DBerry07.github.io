16d768bd8114900ad335f77694715af5d816922f
David Berry <dwjberry07@gmail.com>
2024-02-19 11:22:15 -0500

    Added GitHub link to Hello tab

--- src/data/constants/links.js
+++ src/data/constants/links.js
@@ -1,6 +1,7 @@
 
 export const LINKS = {
     blank: "https://",
+    github: "https://github.com/DBerry07",
     linkedin: "https://linkedin.com/in/david-wj-berry",
     coursera: "https://gitfront.io/r/DWJBerry/WR3GdmWmmRRS/VanderbiltCapstone-Kotlin/",
 
