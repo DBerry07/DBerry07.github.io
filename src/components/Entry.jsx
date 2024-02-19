b4858049732cac3e0f02afd22a89f27264ddfa5f
David Berry <dwjberry07@gmail.com>
2024-02-19 12:36:04 -0500

    Added some conditionals

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -5,37 +5,38 @@ function Entry({ tab, children }) {
   console.log("In Entry = " + tab);
   const data = content[tab];
 
-  const title = data.title;
-  const htmls = data.htmls;
-  const body = data.body;
-  const videos = data.videos;
-
   // console.log("Title: " + title);
   // console.log("HTMLs: " + htmls);
   // console.log("Body: " + body);
   return (
     <div id="content-box">
       {console.log("Generating content for tab: " + tab)}
-      <h2>{title}</h2>
-      {htmls.map((html, index) => (
-        <span class="link">
-          <a href={html[0]} target="_blank" class="link-button">
-            {html[1]}
-          </a>
-        </span>
-      ))}
+      <h2>{tab ? data.title : null}</h2>
+      {tab
+        ? data.htmls.map((html, index) => (
+            <span class="link">
+              <a href={html[0]} target="_blank" class="link-button">
+                {html[1]}
+              </a>
+            </span>
+          ))
+        : null}
       <div>
         <div>
-          {videos.map((video, index) => (
-            <video class="entry-video" width="320" height="540" controls>
-              <source src={video} type="video/mp4" />
-            </video>
-          ))}
+          {tab
+            ? data.videos.map((video, index) => (
+                <video class="entry-video" width="320" height="540" controls>
+                  <source src={video} type="video/mp4" />
+                </video>
+              ))
+            : null}
         </div>
         <div class="entry-body">
-          {body.map((item, index) => (
-            <p>{item}</p>
-          ))}
+          {tab ? (
+            data.body.map((item, index) => <p>{item}</p>)
+          ) : (
+            <p>Invalid selection</p>
+          )}
         </div>
       </div>
     </div>
