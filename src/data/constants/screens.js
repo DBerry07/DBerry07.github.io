92012de856442f062ac11de5dd04fc37a2529812
David Berry <dwjberry07@gmail.com>
2024-02-19 13:23:35 -0500

    Update screens.js

--- src/data/constants/screens.js
+++ src/data/constants/screens.js
@@ -23,7 +23,7 @@ export const Webpage = new MyContent(
   [[LINKS.webpage, "GitHub Repo"]],
   [
     "This webpage (hosted on GitHub Pages) is created with React, set up and tested using Vite.",
-    "JSX, Components, and Hooks were all used to make the user interface and its interactability - as is the norm for React applications.",
+    "JSX, Components, State, and Hooks were all used to make the user interface and its interactability - as is the norm for React applications.",
   ]
 );
 
