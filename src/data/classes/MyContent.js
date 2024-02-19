b502098a75fba3cd0528ffda52067e798055e8ec
David Berry <dwjberry07@gmail.com>
2024-02-19 13:50:16 -0500

    Reformatted

--- src/data/classes/MyContent.js
+++ src/data/classes/MyContent.js
@@ -1,15 +1,18 @@
-class MyContent {    
-    constructor(
-        title = "<title>", 
-        htmls = [["http://", "example"], ["https://", "example2"]], 
-        body = ["This", "is", "an", "empty", "body block."],
-        videos = [],) 
-        {
-        this.title = title;
-        this.htmls = htmls;
-        this.body = body;
-        this.videos = videos;
-    }
+class MyContent {
+  constructor(
+    title = "<title>",
+    htmls = [
+      ["http://", "example"],
+      ["https://", "example2"],
+    ],
+    body = ["This", "is", "an", "empty", "body block."],
+    videos = []
+  ) {
+    this.title = title;
+    this.htmls = htmls;
+    this.body = body;
+    this.videos = videos;
+  }
 }
 
-export default MyContent
+export default MyContent;
--- src/data/classes/Tab.js
+++ src/data/classes/Tab.js
@@ -1,15 +1,14 @@
-
 export class Tab {
-    static Hello = new Tab("Hello");
-    static Webpage = new Tab("Webpage");
-    static Coursera = new Tab("Coursera");
-    static Filler = new Tab("Filler");
+  static Hello = new Tab("Hello");
+  static Webpage = new Tab("Webpage");
+  static Coursera = new Tab("Coursera");
+  static Filler = new Tab("Filler");
 
-    constructor(name) {
-        this.name = name;
-    }
+  constructor(name) {
+    this.name = name;
+  }
 
-    toString() {
-        return this.name;
-    }
+  toString() {
+    return this.name;
+  }
 }
