00865482bfd85fe1c50e66bfaa7838e79e00a256
David Berry <dwjberry07@gmail.com>
2024-02-19 13:24:16 -0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import "../css/Footer.css";
 function Footer() {
   return (
     <footer>
-      <span id="footer-left">Made using React</span>
+      {/* <span id="footer-left">Made using React</span> */}
       <span id="footer-right">Designed and developed by David Berry</span>
     </footer>
   );
