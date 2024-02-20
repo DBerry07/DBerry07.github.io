a08c82660ac410e55a6fcd9d582f01f2d3c68343
David Berry <dwjberry07@gmail.com>
2024-02-20 10:14:55 -0500

    Changed entry data to object rather than MyContent

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -8,27 +8,39 @@ function Entry({ children, tab }) {
   console.log("Selected tab = " + tab);
 
   let data = content[Tab.Hello];
-
   if (Object.keys(Tab).includes(tab)) {
     data = content[tab];
   }
 
-  const title = <h2>{data.title}</h2>;
+  let title = null,
+    htmls = null,
+    videos = null,
+    body = null;
 
-  const htmls = (
-    <div id="link-bar">
-      {data.htmls.map((html, index) => (
-        <span key={html} className="link">
-          <a href={html[0]} target="_blank" className="link-button">
-            {html[1]}
-          </a>
-        </span>
-      ))}
-    </div>
-  );
+  if (Object.hasOwn(data, "title")) {
+    title = <h2>{data.title}</h2>;
+  } else {
+    title = null;
+  }
 
-  const videos =
-    data.videos.length > 0 ? (
+  if (Object.hasOwn(data, "htmls")) {
+    htmls = (
+      <div id="link-bar">
+        {data.htmls.map((html, index) => (
+          <span key={html} className="link">
+            <a href={html[0]} target="_blank" className="link-button">
+              {html[1]}
+            </a>
+          </span>
+        ))}
+      </div>
+    );
+  } else {
+    htmls = null;
+  }
+
+  if (Object.hasOwn(data, "videos")) {
+    videos = (
       <div id="video-section">
         {data.videos.map((video, index) => (
           <video className="entry-video" controls>
@@ -36,15 +48,22 @@ function Entry({ children, tab }) {
           </video>
         ))}
       </div>
-    ) : null;
+    );
+  } else {
+    videos = null;
+  }
 
-  const body = (
-    <div id="entry-body">
-      {data.body.map((item, index) => (
-        <p>{item}</p>
-      ))}
-    </div>
-  );
+  if (Object.hasOwn(data, "body")) {
+    body = (
+      <div id="entry-body">
+        {data.body.map((item, index) => (
+          <p>{item}</p>
+        ))}
+      </div>
+    );
+  } else {
+    body = null;
+  }
 
   return (
     <div id="content-box">
--- src/data/constants/screens.js
+++ src/data/constants/screens.js
@@ -1,13 +1,13 @@
 import MyContent from "../classes/MyContent.js";
 import { LINKS } from "./links.js";
 
-export const Hello = new MyContent(
-  "Hello, there!",
-  [
+export const Hello = {
+  title: "Hello, there!",
+  htmls: [
     [LINKS.linkedin, "Link: LinkedIn"],
     [LINKS.github, "Link: GitHub"],
   ],
-  [
+  body: [
     // Intro page text
     "I am a software developer currently located in South Florida with nearly two years of professional native Android app development experience.",
     "During my professional career, I have worked for Bank of America and for Accenture.",
@@ -15,22 +15,22 @@ export const Hello = new MyContent(
     "I have been described as detail-oriented and analytical.",
     "My passion for software development stems from my enjoyment of puzzle-solving and of building functional things.",
     "I am always eager to learn new things, particularly if those new things help me get better at my job.",
-  ]
-);
+  ],
+};
 
-export const Webpage = new MyContent(
-  "This Webpage",
-  [[LINKS.webpage, "Link: GitHub Repo"]],
-  [
+export const Webpage = {
+  title: "This Webpage",
+  htmls: [[LINKS.webpage, "Link: GitHub Repo"]],
+  body: [
     "This webpage (hosted on GitHub Pages) is created with React, set up and tested using Vite.",
     "JSX, Components, State, and Hooks were all used to make the user interface and its interactability - as is the norm for React applications.",
-  ]
-);
+  ],
+};
 
-export const Coursera = new MyContent(
-  "Coursera Project",
-  [[LINKS.coursera, "Link: GitFront Repo Display"]],
-  [
+export const Coursera = {
+  title: "Coursera Project",
+  htmls: [[LINKS.coursera, "Link: GitFront Repo Display"]],
+  body: [
     // Coursera Project text
     "My project that I submittted to Coursera's Vanderbilt-developed Android developer specialization.",
     "This is a note-taking and -saving app. The user is able to write their own notes and save those notes to their local phone storage." +
@@ -52,13 +52,13 @@ export const Coursera = new MyContent(
       " ReadActivity allows the user to read a given note - but not edit it." +
       "LoggingActivity is the superclass mentioned and is mainly used for logging state changes.",
   ],
-  ["../../src/assets/coursera_video.mp4"]
-);
+  videos: ["../../src/assets/coursera_video.mp4"],
+};
 
-export const Filler = new MyContent(
-  "Filler",
-  [[LINKS.blank, "Hello, World!"]],
-  [
+export const Filler = {
+  title: "Filler",
+  htmls: [[LINKS.blank, "Hello, World!"]],
+  body: [
     "filler",
     "filler",
     "filler",
@@ -103,5 +103,4 @@ export const Filler = new MyContent(
     "filler",
     "filler",
   ],
-  []
-);
+};
