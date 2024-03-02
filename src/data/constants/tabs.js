40d78d3cd6edd9692385a3942f39bbed42c96ec1
David Berry <dwjberry07@gmail.com>
2024-03-02 16:44:57 -0500

    Update tabs.js

--- src/data/constants/tabs.js
+++ src/data/constants/tabs.js
@@ -1,5 +1,6 @@
 /**
  * Note: Property much exactly match String that is assigned to it!
+ * Note: To remove a tab from possible selection on the webpage, comment out (or remove) the tab in question here.
  */
 export const tabs = {
   Hello: "Hello",
