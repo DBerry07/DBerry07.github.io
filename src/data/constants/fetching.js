f443c26ee5c5d7727ce517aa4704bad8b40fe621
David Berry <dwjberry07@gmail.com>
2024-03-03 13:06:28 -0500

    Create fetching.js

---
+++ src/data/constants/fetching.js
@@ -0,0 +1,25 @@
+export async function getText(fileName) {
+  let solution = undefined;
+
+  try {
+    const response = await fetch(fileURL);
+    if (!response.ok) {
+      throw new Error(`Error fetching file: ${response.status}`);
+    }
+
+    const text = await response.text();
+    const lines = text.split("\n"); // Split by newline character
+    solution = lines;
+
+    // Insert newline characters after each line
+    // const formattedText = lines.join('\n');
+
+    // console.log('Formatted text with newlines:');
+    // console.log(formattedText);
+    console.log(lines);
+  } catch (error) {
+    console.error("Error reading local file:", error);
+  }
+
+  return solution;
+}
