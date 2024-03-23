b15f319a393c2f55e014e403cdf93f152fd1d55f
David Berry <dwjberry07@gmail.com>
2024-03-23 19:57:03 -0400

    Changed hello screen link text

--- src/data/constants/screens.js
+++ src/data/constants/screens.js
@@ -5,8 +5,8 @@ import { getText } from "./fetching.js";
 export const Hello = {
   title: "Hello, there!",
   htmls: [
-    [LINKS.linkedin, "[LinkedIn Profile]"],
-    [LINKS.github, "[GitHub Profile]"],
+    [LINKS.linkedin, "[LinkedIn]"],
+    [LINKS.github, "[GitHub]"],
   ],
   subheading: "Click on the buttons to navigate!",
   body: await getText(locations.HelloBody), // "await" here is very important! Page won't work without it!
