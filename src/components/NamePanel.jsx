95c9fd6fa68ba0f29bbaedea41ddaf078f724765
David Berry <dwjberry07@gmail.com>
2024-03-23 22:33:53 -0400

    Stylizing

--- src/components/NamePanel.jsx
+++ src/components/NamePanel.jsx
@@ -1,5 +1,9 @@
 import "../css/NamePanel.css";
 
 export default function NamePanel() {
-  return <div id="namepanel">David WJ Berry</div>;
+  return (
+    <div id="namepanel">
+      David <span id="middle-initals">WJ</span> Berry
+    </div>
+  );
 }
