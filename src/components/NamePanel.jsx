ff635af8ccf3d114686182cfad831cc1fd3f15a4
David Berry <dwjberry07@gmail.com>
2024-03-27 15:43:35 -0400

    Moved linkedin & github profile links to namepanel

--- src/components/NamePanel.jsx
+++ src/components/NamePanel.jsx
@@ -1,9 +1,23 @@
 import "../css/NamePanel.css";
+import { LINKS } from "../data/constants/links.js";
+import { locations } from "../data/constants/filelocs.js";
 
 export default function NamePanel() {
+  var size = 25;
+
   return (
     <div id="namepanel">
-      David <span id="middle-initals">WJ</span> Berry
+      <div id="name">
+        David <span id="middle-initals">WJ</span> Berry
+      </div>
+      <div>
+        <a href={LINKS.linkedin} target="_blank" className="job-link">
+          <img src={locations.PicLinkedin} width={size} height={size} />
+        </a>
+        <a href={LINKS.github} target="_blank" className="job-link">
+          <img src={locations.PicGithub} width={size} height={size} />
+        </a>
+      </div>
     </div>
   );
 }
--- src/data/constants/screens.js
+++ src/data/constants/screens.js
@@ -4,10 +4,6 @@ import { getText } from "./fetching.js";
 
 export const Hello = {
   title: "Hello, there!",
-  htmls: [
-    [LINKS.linkedin, locations.PicLinkedin],
-    [LINKS.github, locations.PicGithub],
-  ],
   subheading: "Click on the buttons to navigate!",
   body: await getText(locations.HelloBody), // "await" here is very important! Page won't work without it!
 };
