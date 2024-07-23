da0df7203c93ea5ee78762186a86228277b10ef7
David W. Berry <david.wj.berry@gmail.com>
2024-07-23 17:33:56 -0400

    added ability for title in lists

--- src/components/entry/List.jsx
+++ src/components/entry/List.jsx
@@ -2,8 +2,19 @@ import "./list.css";
 
 export default function List({ list }) {
   if (list != null) {
+
+    var title;
+    if (list.at(0).at(0) != '-') {
+      title = list.at(0);
+      list = list.slice(1, list.length - 1);
+    } else {
+      title = ""
+    }
+
     return (
+      
       <ul>
+        {title}
         {list.map((item, index) => (
           <li key={index}>{item}</li>
         ))}
