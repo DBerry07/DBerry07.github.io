798f75698ef4f52b2893cca1a3fd03b7e0de849f
David W. Berry <david.wj.berry@gmail.com>
2024-07-30 18:41:36 -0400

    changed project
    Now is TypeScript and will use the Material UI.

--- src/data/constants/links.js
+++
@@ -1,8 +0,0 @@
-
-export const LINKS = {
-    blank: "https://",
-    github: "https://github.com/DBerry07",
-    linkedin: "https://linkedin.com/in/david-wj-berry",
-    coursera: "https://gitfront.io/r/DWJBerry/WR3GdmWmmRRS/VanderbiltCapstone-Kotlin/",
-
-}
