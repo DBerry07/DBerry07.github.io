0dfffaff34d88c5b48c29a06e0404c03f34c96d3
David W. Berry <david.wj.berry@gmail.com>
2024-07-30 16:02:54 -0400

    added placeholder body

--- src/data/screens/projects_more.js
+++ src/data/screens/projects_more.js
@@ -2,11 +2,17 @@ import Subheading from "../../components/entry/Subheading"
 import Title from "../../components/entry/Title"
 import EntryBody from "../../components/entry/EntryBody"
 import EntryBox from "../../components/entry/EntryBox"
+import Body from "../../components/entry/Body"
+import { locations } from "../constants/filelocs"
+import { getText } from "../constants/fetching"
 
 export const MoreProjects = [
     Title({title: "Additional Projects"}),
     EntryBox({items:[
             Subheading({subheading: ["Additional projects I have worked on"]}),
+            EntryBody({items:[
+                Body({ body: ["Nothing here yet,", "but keep an eye out!"] }),
+            ]}),
         ]
     })
 ]
