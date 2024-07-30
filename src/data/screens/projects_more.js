990fdadab13d28c651c059bbebde60c274f2bdb4
David W. Berry <david.wj.berry@gmail.com>
2024-07-30 15:58:22 -0400

    Create projects_more.js

---
+++ src/data/screens/projects_more.js
@@ -0,0 +1,12 @@
+import Subheading from "../../components/entry/Subheading"
+import Title from "../../components/entry/Title"
+import EntryBody from "../../components/entry/EntryBody"
+import EntryBox from "../../components/entry/EntryBox"
+
+export const MoreProjects = [
+    Title({title: "Additional Projects"}),
+    EntryBox({items:[
+            Subheading({subheading: ["Additional projects I have worked on"]}),
+        ]
+    })
+]
