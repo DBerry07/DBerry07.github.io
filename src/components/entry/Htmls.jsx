cc9ff07e5243dcece728824b963dd424139ac684
David W. Berry <david.wj.berry@gmail.com>
2024-11-25 17:24:09 -0500

    shrink hyperlink to size of image

--- src/components/entry/Htmls.jsx
+++ src/components/entry/Htmls.jsx
@@ -3,15 +3,17 @@
 export default function Htmls({ htmls }) {
   if (htmls != null) {
     return (
-      <div id="link-bar">
+        <div className="w-full items-center">
+      <div id="link-bar" className="p-2 w-fit">
         {htmls.map((html, index) => (
           <span key={index} className="link">
-            <a href={html[0]} target="_blank">
-              <img src={html[1]} className="link-img" />
+            <a href={html[0]} target="_blank" className="w-0">
+              <img src={html[1]} className="link-img h-6" />
             </a>
           </span>
         ))}
       </div>
+        </div>
     );
   }
 }
