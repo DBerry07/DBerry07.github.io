465f058e76cc751c48973f91ee5d600bdb6c0e1e
David W. Berry <david.wj.berry@gmail.com>
2024-11-26 13:00:50 -0500

    Reformat code

--- src/components/entry/Htmls.jsx
+++ src/components/entry/Htmls.jsx
@@ -1,19 +1,17 @@
 // import "./htmls.css";
 
-export default function Htmls({ htmls }) {
-  if (htmls != null) {
-    return (
-        <div className="w-full items-center">
-      <div id="link-bar" className="p-2 w-fit">
-        {htmls.map((html, index) => (
-          <span key={index} className="link">
-            <a href={html[0]} target="_blank" className="w-0">
-              <img src={html[1]} className="link-img h-6" />
-            </a>
-          </span>
-        ))}
-      </div>
-        </div>
-    );
-  }
+export default function Htmls({htmls}) {
+    if (htmls != null) {
+        return (
+                <div id="link-bar" className="w-full  place-items-center">
+                    {htmls.map((html, index) => (
+                        <span key={index} className="link m-2">
+                          <a href={html[0]} target="_blank" className="w-0">
+                            <img src={html[1]} className="link-img h-6"/>
+                          </a>
+                        </span>
+                    ))}
+                </div>
+        );
+    }
 }
