136811121ad28064f897f856c8a8bec9579cc233
David W. Berry <david.wj.berry@gmail.com>
2024-11-26 13:01:50 -0500

    Make entry box scrollable

--- src/components/entry/EntryBox.jsx
+++ src/components/entry/EntryBox.jsx
@@ -2,7 +2,7 @@ import { Fragment } from "react";
 
 export default function EntryBox(props) {
     return (
-        <div id="content-box" className="row-span-6 h-full w-full text-lg">
+        <div id="content-box" className="row-span-6 overflow-y-scroll mb-20 h-full w-full text-lg grid grid-cols-1 grid-rows-8">
             {props.items.map((item, index) =>
             <Fragment key={index}>
             {item}
