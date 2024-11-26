50f06eab8783f3824972c7798bd126447539d341
David W. Berry <david.wj.berry@gmail.com>
2024-11-26 17:16:46 -0500

    Center links bar

--- src/components/entry/Htmls.jsx
+++ src/components/entry/Htmls.jsx
@@ -3,13 +3,11 @@
 export default function Htmls({htmls}) {
     if (htmls != null) {
         return (
-                <div id="link-bar" className="w-full h-fit justify-center flex flex-row">
+                <div id="link-bar" className="w-full h-full  justify-center place-items-center flex flex-row">
                     {htmls.map((html, index) => (
-                        <span key={index} className="link ml-4 mr-4">
-                          <a href={html[0]} target="_blank">
+                          <a href={html[0]} target="_blank" className='ml-4 mr-4'>
                             <img src={html[1]} className="h-10 sepia transition hover:scale-125"/>
                           </a>
-                        </span>
                     ))}
                 </div>
         );
