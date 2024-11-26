2eb98eea13a483bb1a0ccfacb7cefab00dd86800
David W. Berry <david.wj.berry@gmail.com>
2024-11-26 16:47:04 -0500

    Remove bottom margin, was interfering with personal links in name bar

--- src/components/entry/Entry.jsx
+++ src/components/entry/Entry.jsx
@@ -4,7 +4,7 @@ import { Fragment } from "react";
 function Entry(props) {
   
   return (
-    <div id="entry" className="row-span-6 mb-20 gap-4 col-span-5 grid grid-cols-1 grid-rows-7 h-full w-full place-items-center">
+    <div id="entry" className="row-span-6 col-span-5 grid grid-cols-1 grid-rows-7 h-full w-full place-items-center">
       { props.content.map((item, index) =>
       <Fragment key={index}>
       {item}
