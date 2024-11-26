16aa90425d880dd69e18943b1f3201c5fe6a69c9
David W. Berry <david.wj.berry@gmail.com>
2024-11-26 13:01:33 -0500

    Row span entry body

--- src/components/entry/EntryBody.jsx
+++ src/components/entry/EntryBody.jsx
@@ -2,7 +2,7 @@ import { Fragment } from "react";
 
 export default function EntryBody(props) {
     return (
-        <div id="entry-body">
+        <div id="entry-body" className='row-span-6 m-2'>
             {props.items.map((item, index) =>
             <Fragment key={index}>
                 {item}
