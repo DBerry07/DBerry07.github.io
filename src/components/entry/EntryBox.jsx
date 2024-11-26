a8f0d9b9dc931c54177413d6f5065d30712ebb75
David W. Berry <david.wj.berry@gmail.com>
2024-11-26 17:08:22 -0500

    Sort of fixed cutting off of entry box bottom

--- src/components/entry/EntryBox.jsx
+++ src/components/entry/EntryBox.jsx
@@ -2,7 +2,7 @@ import { Fragment } from "react";
 
 export default function EntryBox(props) {
     return (
-        <div id="content-box" className="row-span-6 overflow-y-scroll mb-10 h-full w-full text-lg grid grid-cols-1 grid-rows-8">
+        <div id="content-box" className="row-span-5 overflow-y-scroll h-full w-full text-lg grid grid-cols-1 grid-rows-6">
             {props.items.map((item, index) =>
             <Fragment key={index}>
             {item}
