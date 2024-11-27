6865026777b3be1e58dceb1a6653244788ad3819
David W. Berry <david.wj.berry@gmail.com>
2024-11-27 10:35:24 -0500

    Cleaning up

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,18 @@
-import {useEffect, useState, Fragment} from "react";
-import {CSSTransition} from "react-transition-group";
+import {useState, Fragment} from "react";
 import pageContent from "./data/constants/content.js";
 import Entry from "./components/entry/Entry.jsx";
 import ButtonMenu from "./components/ButtonMenu.jsx";
 import myTabs from "./data/constants/my-tabs.js";
-import {panels} from "./data/constants/panels.js";
 import NamePanel from "./components/NamePanel.jsx";
 import Footer from "./components/Footer.jsx";
 
 function App() {
     const [tab, setTab] = useState(myTabs.Hello.name);
-    const [panel, setPanel] = useState(panels.About);
     const content = pageContent[tab];
 
 
     function handleSelect(value) {
-        changePanel(value);
-    }
-
-    function changePanel(value) {
-        if (value === myTabs.Projects) {
-            setPanel(panels.Projects);
-            setTab(myTabs.ProjectList[1].name);
-        } else if (value === myTabs.Back) {
-            setPanel(panels.About);
-            setTab(myTabs.Panels[0].name);
-        } else {
-            setTab(value);
-        }
+        setTab(value);
     }
 
     return (
@@ -36,7 +21,7 @@ function App() {
                 <ButtonMenu onSelect={handleSelect}/>
                 <NamePanel/>
                 <Entry content={content}/>
-                <Footer/>
+                {/*<Footer/>*/}
             </main>
         </Fragment>
     );
--- src/data/constants/panels.js
+++
@@ -1,4 +0,0 @@
-export const panels = {
-  About: "About",
-  Projects: "Projects",
-};
